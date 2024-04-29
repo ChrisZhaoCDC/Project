84c455b14428da71717a5a07d038113947c3178f
ChrisZhaoCDC <148534346+ChrisZhaoCDC@users.noreply.github.com>
2024-04-29 17:10:32 +0800

    GUI.py

--- GUI.py
+++ GUI.py
@@ -13,13 +13,13 @@ from PIL import Image as PILImage
 import tkinter as tk
 import ttkbootstrap as ttk
 from matplotlib import pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg#绘图显示
-from tkinter import filedialog#选择文件
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg#draw
+from tkinter import filedialog#select folder
 from tkinter import messagebox
 import torch.nn as nn
 from torchvision.transforms import transforms
 import torch.nn.functional as F
-from tkinter import filedialog#选择文件
+from tkinter import filedialog
 import os
 classNames = ['apple_pie', 'cheesecake', 'chicken_curry', 'french_fries', 'fried_rice', 'hamburger', 'hot_dog', 'ice_cream', 'omelette', 'pizza', 'sushi']
 
@@ -37,12 +37,12 @@ def chooseModel():
 
     win.title(modelName)
 # 设置模型为评估模式
-Transformer = transforms.Compose([transforms.Resize((224, 224)),##所有同意尺寸为宽高224
-                                     transforms.ToTensor(),#将图片转为张量
-                                     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])#进行归一化
+Transformer = transforms.Compose([transforms.Resize((224, 224)),##make all resize 224
+                                     transforms.ToTensor(),
+                                     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])#Normalize
 device = torch.device('cpu')
 
-# 定义识别函数
+# recognize function
 def identify():
     global answer, outputs
     filePath = filedialog.askopenfilename()
@@ -51,7 +51,7 @@ def identify():
     ax.imshow(img)
     ax.set_xticks([])
     ax.set_yticks([])
-    canvas = FigureCanvasTkAgg(fig, master=win)#设置放置画布
+    canvas = FigureCanvasTkAgg(fig, master=win)#set background
     canvas.draw()
     canvas.get_tk_widget().place(x=100, y=10)
     
@@ -64,10 +64,10 @@ def identify():
     answer = classNames[predicted]
     outputs = outputs[0, :]
 
-win = ttk.Window(title='Image Recognition')#实例化窗口
-PCWidth = win.winfo_screenwidth()#获取屏幕宽
-PCHeight = win.winfo_screenheight()#获取屏幕高
-win.geometry('500x430+{}+{}'.format(int(PCWidth/4),int(PCHeight/4)))#设置窗口尺寸
+win = ttk.Window(title='Image Recognition')#Instantiation window
+PCWidth = win.winfo_screenwidth()#Get screen width
+PCHeight = win.winfo_screenheight()#Get screen tall
+win.geometry('500x430+{}+{}'.format(int(PCWidth/4),int(PCHeight/4)))#set windows size
 
 selectButton = tk.Button(win, text='Select Image', command=identify)
 selectButton.place(x=170, y=330+30, width=150, height=40)
